f7c91587ddd2d1e71546c94dd2f56bf87f9236bd
Ayush Kumar <ayushkumar@Ayushs-Laptop.local>
2024-12-21 00:11:42 +0530

    fixed login thing

--- app/app/user/views.py
+++ app/app/user/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, authentication, permissions
 from user.serializers import (UserSerializer, AuthTokenSerializer)
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import AllowAny
 
 
 
@@ -12,6 +13,8 @@ class UserView(generics.CreateAPIView):
 class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_class = api_settings.DEFAULT_RENDERER_CLASSES
+    authentication_classes = []
+    permission_classes= [AllowAny]
 
 class UpdateUserView(generics.RetrieveUpdateAPIView):
 
